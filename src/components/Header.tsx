7fba8f06c1466611671eb2c3d894ad4b3091217c
marcboreu <marcboreu@gmail.com>
2023-04-23 18:26:31 +0200

    feat: add content for header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,37 @@
-export const Header = () => {
-  return <div>Header</div>;
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { APP_NAME } from "../utils/globals";
+
+const Header = (): JSX.Element => {
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 250);
+
+    return () => clearTimeout(timer);
+  }, [location, isLoading]);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setIsLoading(true);
+    };
+    window.addEventListener("popstate", handlePopState);
+    return () => window.removeEventListener("popstate", handlePopState);
+  }, []);
+
+  return (
+    <header className="header">
+      <Link to="/" onClick={() => setIsLoading(true)}>
+        <h1>{APP_NAME}</h1>
+      </Link>
+      <div className="loader-container">
+        {isLoading && <span className="loader" />}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
