5829f1dc8c4d54c9f933ec6b2d6ec552dac93329
marcboreu <marcboreu@gmail.com>
2023-04-23 14:41:36 +0200

    feat: add globals params and copies

---
+++ src/utils/globals.ts
@@ -0,0 +1,24 @@
+export const EXPIRETIME: number = 1000 * 60 * 60 * 24; // 1 day
+export const LIMIT_RESULTS = 100;
+export const APP_NAME: string = "Podcaster";
+
+
+// COPIES
+export const NUMBER_RESULTS: string = LIMIT_RESULTS.toString();
+export const NO_RESULTS: string = "No results found";
+export const FILTER_PODCASTS: string = "Filter podcasts...";
+export const NO_RESULTS_FOR: string = "No results for: ";
+export const NO_RESULTS_PODCASTS: string = "No podcasts found";
+export const NO_RESULTS_EPISODES: string = "No episodes found";
+export const LOADER_PODCASTS_MAIN: string = `Loading ${NUMBER_RESULTS} popular podcasts ...`;
+export const LOADER_PODCASTS_EPISODES: string = "Loading podcasts episodes ...";
+export const PODCASTS_AUTHOR: string = "Author: ";
+export const PODCASTS_EPISODES: string = "Episodes: ";
+export const PODCASTS_EPISODES_TITLE: string = "Title";
+export const PODCASTS_EPISODES_DURATION: string = "Duration";
+export const PODCASTS_EPISODES_DATE: string = "Date";
+export const PODCASTS_BY: string = "by ";
+export const PODCASTS_DESCRIPTION: string = "Description: ";
+export const ERROR_MESSAGE_PAGE_NOTFOUND: string = "404: Page not found";
+export const ERROR_MESSAGE_PAGE_NOTFOUND_SUBTITLE: string =
+  "Redirecting to home page in 2 seconds...";
