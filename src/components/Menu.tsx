8ce31f1bba3e6cf6791b43052fc414deac710aa1
marcboreu <marcboreu@gmail.com>
2023-04-23 18:26:00 +0200

    feat: add content for menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,25 @@
-export const Menu = () => {
-  return <div>Menu</div>;
+import { usePodcasts } from "../context/PodcastContext";
+import { FILTER_PODCASTS, NUMBER_RESULTS } from "../utils/globals";
+
+const Menu = () => {
+  const { handleSearch, searchTerm } = usePodcasts();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    handleSearch(value);
+  };
+
+  return (
+    <nav className="search">
+      <span>{NUMBER_RESULTS}</span>
+      <input
+        onChange={handleChange}
+        value={searchTerm}
+        name="query"
+        placeholder={FILTER_PODCASTS}
+      />
+    </nav>
+  );
 };
 
 export default Menu;
