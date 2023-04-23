420245df5343acec32ad9d5a45b28f771183f989
marcboreu <marcboreu@gmail.com>
2023-04-23 18:28:48 +0200

    feat: add content for LoaderPodcasts component

--- src/components/LoaderPodcasts.tsx
+++ src/components/LoaderPodcasts.tsx
@@ -1,5 +1,18 @@
-export const LoaderPodcasts = () => {
-  return <div>LoaderPodcasts</div>;
+import { FC } from "react";
+
+interface Props {
+  text: string;
+}
+
+const LoaderPodcasts: FC<Props> = ({ text }) => {
+  return (
+    <div className="loader-wrapper">
+      <span>{text}</span>
+      <div className="loader-podcasts">
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
 };
 
 export default LoaderPodcasts;
