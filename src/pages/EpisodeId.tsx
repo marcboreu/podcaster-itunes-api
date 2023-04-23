951ddc623cd99f92f82ff921a28bd3bfb051b66d
marcboreu <marcboreu@gmail.com>
2023-04-23 18:31:14 +0200

    feat: add content for episodeId page

--- src/pages/EpisodeId.tsx
+++ src/pages/EpisodeId.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { FC } from "react";
+import { useLocation } from "react-router-dom";
+import { formatHttpsLinks } from "../utils/helpers";
 
-export const EpisodeId = () => {
+interface EpisodeProps {}
+
+const EpisodeId: FC<EpisodeProps> = () => {
+  const location = useLocation();
+  const podcast = location.state;
+
   return (
-    <div>EpisodeId</div>
-  )
-}
-
-export default EpisodeId
+    <div className="episode">
+      <p>{podcast.trackName}</p>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: formatHttpsLinks(podcast.description),
+        }}
+      />
+      <audio className="soundbar" controls>
+        <source
+          data-testid="source-soundbar"
+          src={podcast.episodeUrl}
+          type="audio/mpeg"
+        />
+      </audio>
+    </div>
+  );
+};
+export default EpisodeId;
