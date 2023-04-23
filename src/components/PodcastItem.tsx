34c097fed0c60bba38a0f0d13330fe5b92e6dd5b
marcboreu <marcboreu@gmail.com>
2023-04-23 18:28:26 +0200

    feat: add content for PodcastItem component

--- src/components/PodcastItem.tsx
+++ src/components/PodcastItem.tsx
@@ -1,5 +1,25 @@
-export const PodcastItem = () => {
-  return <div>PodcastItem</div>;
+import { Link } from "react-router-dom";
+import { PODCASTS_AUTHOR } from "../utils/globals";
+
+interface PodcastItemProps {
+  image: { label: string };
+  song: { label: string };
+  author: { label: string };
+  id: string;
+}
+
+const PodcastItem = ({ image, song, author, id }: PodcastItemProps) => {
+  return (
+    <Link to={`/podcast/${id}`}>
+      <li className="podcast">
+        <img src={image.label} alt={author.label} />
+        <div className="podcast-content">
+          <h4>{song.label}</h4>
+          <p>{PODCASTS_AUTHOR}{author.label}</p>
+        </div>
+      </li>
+    </Link>
+  );
 };
 
 export default PodcastItem;
