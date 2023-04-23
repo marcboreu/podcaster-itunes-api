e98c97c09d85b6bdf31ff51c8427b3621864a9e1
marcboreu <marcboreu@gmail.com>
2023-04-23 17:40:22 +0200

    feat: add 404 page content

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  ERROR_MESSAGE_PAGE_NOTFOUND,
+  ERROR_MESSAGE_PAGE_NOTFOUND_SUBTITLE,
+} from "../utils/globals";
 
-export const NotFound = () => {
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
-    <div>404</div>
-  )
-}
+    <div>
+      <h1>{ERROR_MESSAGE_PAGE_NOTFOUND}</h1>
+      <p>{ERROR_MESSAGE_PAGE_NOTFOUND_SUBTITLE}</p>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
